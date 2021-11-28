be1a7ae0a0ad2fb78e28fac43094c2376a83fc1e
Alek <aleksebastian@gmail.com>
2021-11-27 18:06:09 -0600

    Basic todo functionality

---
+++ index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+// import * as serviceWorker from './serviceWorker';
+import reportWebVitals from "./reportWebVitals";
+import store from "./app/store";
+import "./index.css";
+
+function render() {
+  const App = require("./app/App").default;
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+}
+
+render();
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./app/App", render);
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
